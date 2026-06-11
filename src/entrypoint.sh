47eff2bbac9aafd9f93c617e047fe5425ef0f2b4
Kroese <kroese@users.noreply.github.com>
2026-06-11 16:56:27 +0200

    feat: Hide supercronic messages

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -149,7 +149,7 @@ read -r POSTFIX_PID < /var/spool/postfix/pid/master.pid
 
 echo "Starting supercronic..."
 echo "30 2 * * * $dir/proxmox-datacenter-manager-daily-update 2>&1 | tee -a /tmp/daily.log" > /docker.cron
-supercronic /docker.cron &
+supercronic -quiet -no-reap /docker.cron &
 CRON_PID=$!
 
 # Start rsyslog
