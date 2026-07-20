b77c2218d2a64e35859bd4384eab9386c67383e1
Kroese <kroese@users.noreply.github.com>
2026-07-20 15:11:56 +0200

    fix: Resolve shellcheck warnings (#14)

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -724,13 +724,14 @@ EOF
 supercronic -quiet -no-reap /docker.cron &
 CRON_PID=$!
 
-# Trap helper
 _trap() {
   local func="$1"; shift
   local sig
   TRAP_PID=$BASHPID
 
   for sig; do
+    # Capture the local callback and signal while registering the trap.
+    # shellcheck disable=SC2064
     trap "$func $sig" "$sig"
   done
 
